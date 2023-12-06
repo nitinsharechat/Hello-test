d4d699b914e8da9efc1d7f1d45d3a561c86d1e28
Nitin <nitinkumar@sharechat.co>
2023-12-06 11:22:03 +0530

    added test.java

---
+++ test.java
@@ -0,0 +1,5 @@
+String sql = "SELECT * FROM users WHERE username=? AND password=?";
+PreparedStatement pstmt = conn.prepareStatement(sql);
+pstmt.setString(1, username);
+pstmt.setString(2, password);
+ResultSet rs = pstmt.executeQuery();
